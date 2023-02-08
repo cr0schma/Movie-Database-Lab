73b1b97b2dbe98ced9cc09db98ac3ff563d1108a
Chris Schmalz <cschmalz03@gmail.com>
2023-02-07 21:11:24 -0500

    initial commit

---
+++ MovieDatabase/Movies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase
+{
+    public class Movies
+    {
+        public string movieTitle;
+        public string movieCategory;
+
+        public Movies(string title, string category) {
+            movieTitle = title;
+            movieCategory = category;
+        }
+    }
+}
---
+++ MovieDatabase/Program.cs
@@ -0,0 +1,53 @@
+﻿using MovieDatabase;
+using System.Reflection;
+using System.Linq;
+
+string title;
+string category;
+
+List<Movies> movies = new()
+{
+    new Movies(title = "Scream", category = "horror" ),
+    new Movies(title = "The Ring", category = "horror"),
+    new Movies(title = "Saw", category = "horror"),
+    new Movies(title = "The Lion King", category = "animated"),
+    new Movies(title = "Shrek", category = "animated"),
+    new Movies(title = "Cars", category = "animated"),
+    new Movies(title = "Gladiator", category = "drama"),
+    new Movies(title = "Good Will Hunting", category = "drama"),
+    new Movies(title = "The Dark Knight", category = "drama"),
+    new Movies(title = "Guardians of the Galaxy", category = "scifi")
+
+};
+
+Console.WriteLine("Welcome to the Movie List Application!\n");
+Console.WriteLine($"There are {movies.Count} movies in this list.");
+
+bool playagain = true;
+while (playagain)
+{
+    Console.Write("What category are you interested in?: ");
+    string userCategory = Console.ReadLine();
+
+    var userMovies = movies.Where(x => x.movieCategory == userCategory);
+
+    if (userMovies.Count() > 0)
+    {
+        foreach (var movie in userMovies)
+        {
+            Console.WriteLine(movie.movieTitle);
+        }
+    }
+    else
+    {
+        Console.WriteLine("That is an invalid category");
+    }
+
+    Console.Write("Continue ? (y/n): ");
+    string userDecision = Console.ReadLine();
+
+    if (userDecision == "n")
+    {
+        playagain = false;
+    }
+}
